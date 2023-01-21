a420991118cc10f5de785a1177535d6505a61020
krates98 <krates198@gmail.com>
2023-01-21 19:18:34 +0530

    finishing values

--- src/pages/Home/Heading.js
+++ src/pages/Home/Heading.js
@@ -1,16 +1,29 @@
 import React from "react";
 import "./Styles/heading.css";
+import AOS from "aos";
+import "aos/dist/aos.css";
+
+AOS.init();
 const Heading = () => {
   return (
-    <div className="titleBox">
-      <h1 className="display-1">Amplify Your Business With PayGoal!</h1>
-      <h5>
-        The fastest and easiest way to collect payments and make payouts for
-        growing business
-      </h5>
-      <button type="button" className="btn btn-primary btn-lg getstarted">
-        Get Started
-      </button>
+    <div className="heading">
+      <div className="titleBox">
+        <h1 className="display-1" data-aos="flip-left" data-aos-duration="1500">
+          Amplify Your Business With PayGoal!
+        </h1>
+        <h5>
+          The fastest and easiest way to collect payments and make payouts for
+          growing business
+        </h5>
+        <button
+          type="button"
+          className="btn btn-primary btn-lg getstarted"
+          data-aos="flip-right"
+          data-aos-duration="1500"
+        >
+          Get Started
+        </button>
+      </div>
     </div>
   );
 };
--- src/pages/Home/Vision.js
+++ src/pages/Home/Vision.js
@@ -1,10 +1,41 @@
 import React from "react";
-import "./Styles/vision";
+import AOS from "aos";
+import "aos/dist/aos.css";
+import "./Styles/vision.css";
+
+AOS.init();
 
 const Vision = () => {
   return (
     <div className="vision">
-      <h1 className="display-2">Vision</h1>
+      <div
+        className="visionHeading"
+        data-aos="fade-left"
+        data-aos-duration="1500"
+      >
+        <h1 className="display-2">Vision & Values</h1>
+      </div>
+      <div class="container text-center mt-2 container">
+        <div class="row row-cols-auto">
+          <div class="col-md" data-aos="fade-left" data-aos-duration="1500">
+            <img src="/images/vision.png" width="100%" />
+          </div>
+          <div
+            class="col-md d-flex align-items-center"
+            data-aos="fade-right"
+            data-aos-duration="1500"
+          >
+            <p>
+              Accept All Payment Modes with our Powerful Dashboard Quickly go
+              live with PayGoal and experience the future of payments. Accept
+              domestic and international payments with 100+ payment options.
+              Choose from a range of integrations to give the best checkout
+              experience to you customers. PayGoal works on any device with the
+              PayGoal API, iOS, and Android SDK.
+            </p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
